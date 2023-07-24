9edbeaf441aa3ab482e910690ab427c1c60d8a58
dolsen0 <derek.olsen@gmail.com>
2023-07-24 16:00:44 -0400

    displaying all info and communitcating to api OR express app

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
-import Image from 'next/image'
+import React from 'react'
 import styles from './page.module.css'
 import GetShellyInfo from '@/components/GetShellyInfo'
+
 
 export default function Home() {
   return (
--- src/components/GetShellyInfo.jsx
+++ src/components/GetShellyInfo.jsx
@@ -1,8 +1,44 @@
+'use client'
+import React, {useState, useEffect} from 'react';
+
 export default function GetShellyInfo(){
+    const [shellyInfo, setShellyInfo] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [id, setId] = useState("")
+    const [source, setSource] = useState("")
+    const [output, setOutput] = useState("")
+    const [apower, setApower] = useState("")
+    const [voltage, setVoltage] = useState("")
+    
+    
+    useEffect(() => {
+        // fetch('http://192.168.33.1/rpc/Shelly.GetStatus')
+        fetch('http://localhost:3000/home')
+        .then(res => res.json())
+        .then(data => {
+        let shellyInfo  = (data['switch:0'])
+        setId(shellyInfo['id'])
+        setSource(shellyInfo['source'])
+        setOutput(shellyInfo['output'])
+        setApower(shellyInfo['apower'])
+        setVoltage(shellyInfo['voltage'])
+
+        setShellyInfo(shellyInfo)
+        setLoading(false)
+        
+        console.log(voltage)
+            
+        })
+        .catch(err => console.log(err))
+    }
+    ,[])
+ 
     return(
         <div>
-            <h1>Shelly Info Goes Here</h1>
+            {
+            loading ? <h3>Loading...</h3> : 
+            <p>Voltage:{voltage} Id: {id} Source: {source} output{output} apower: {apower}</p>
+            }
         </div>
     )
 }
-
