9af22c895548c50173c58ad33d8c6e130747c626
dolsen0 <derek.olsen@gmail.com>
2023-07-26 15:40:01 -0400

    added error handling via catch on GetRestart component

--- src/components/GetRestart.jsx
+++ src/components/GetRestart.jsx
@@ -5,6 +5,7 @@ export default function GetRestart() {
         <button className="btn__restart" onClick={()=>{
             // fetch('http://192.168.33.1/rpc/Switch.Toggle?id=0')
             fetch('http://localhost:3000/home/restart')
+            .catch(err => console.log(err))
         }}>Restart</button>
     )
 }
