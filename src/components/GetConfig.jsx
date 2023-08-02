a7fdbd4963b20c4e53c0d60d053874537b77be0e
dolsen0 <derek.olsen@gmail.com>
2023-08-02 15:45:33 -0400

    updated style for form and buttons

--- src/components/GetConfig.jsx
+++ src/components/GetConfig.jsx
@@ -1,7 +1,30 @@
-export default function GetConfig(){
-    return(
-        <div>
-        <h3>Configure New Devices</h3>
-        </div>
-    )
+export default function GetConfig() {
+  return (
+    <section className="getConfig">
+      <h3>Configure New Devices</h3>
+      <div className="flex">
+        <form action="/home/config" method="POST">
+          <div>
+            <label for="ssid">SSID:</label>
+            <input
+              type="text"
+              id="ssid"
+              name="ssid"
+              placeholder="MyWifiName!"
+            ></input>
+          </div>
+          <div>
+            <label for="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Shh-Don't-Tell!"
+            ></input>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
 }
