51fe7f5edf252501f504339851213cf1376974d3
dolsen0 <derek.olsen@gmail.com>
2023-08-08 14:47:12 -0400

    added htmlFor in GetConfid

--- src/components/GetConfig.jsx
+++ src/components/GetConfig.jsx
@@ -1,11 +1,38 @@
+import React from 'react';
+
 export default function GetConfig() {
+  const handleSubmit = async (event) => {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    const ssid = event.target.ssid.value;
+    const pass = event.target.password.value;
+
+    try {
+      const response = await fetch('/home/config', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ssid, pass }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+      // Handle success, maybe navigate the user or show a success message
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle errors, maybe show an error message to the user
+    }
+  };
+
   return (
     <section className="getConfig">
       <h3>Configure New Devices</h3>
       <div className="flex">
-        <form action="/home/config" method="POST">
+        <form onSubmit={handleSubmit}>
           <div>
-            <label for="ssid">SSID:</label>
+            <label htmlFor="ssid">SSID:</label>
             <input
               type="text"
               id="ssid"
@@ -14,7 +41,7 @@ export default function GetConfig() {
             ></input>
           </div>
           <div>
-            <label for="password">Password:</label>
+            <label htmlFor="password">Password:</label>
             <input
               type="password"
               id="password"
